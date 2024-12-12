5e24e1ac24c6b9eac0498b7a839b6425f822d20f
Chiradip Maji <chiradip.svts@gmail.com>
2024-12-12 09:21:52 +0530

    Adds interface for repository

--- src/main/java/com/khorcha/repository/UsersRepository.java
+++ src/main/java/com/khorcha/repository/UsersRepository.java
@@ -11,40 +11,11 @@ import software.amazon.awssdk.services.dynamodb.model.DynamoDbException;
 import java.util.Optional;
 
 @Singleton
-public class UsersRepository {
+public interface UsersRepository {
 
-    private final DynamoDbTable<Users> usersTable;
+    void saveUser(Users user);
 
-    public UsersRepository(DynamoDbClient dynamoDbClient) {
-        DynamoDbEnhancedClient enhancedClient = DynamoDbEnhancedClient.builder()
-                .dynamoDbClient(dynamoDbClient)
-                .build();
+    Optional<Users> getUserByUsername(String username);
 
-        this.usersTable = enhancedClient.table("Users", TableSchema.fromBean(Users.class));
-    }
-
-    public void saveUser(Users user) {
-        try {
-            usersTable.putItem(user);
-        } catch (DynamoDbException e) {
-            throw new RuntimeException("Failed to save user", e);
-        }
-    }
-
-    public Optional<Users> getUserByUsername(String username) {
-        try {
-            Users user = usersTable.getItem(r -> r.key(k -> k.partitionValue(username)));
-            return Optional.ofNullable(user);
-        } catch (DynamoDbException e) {
-            throw new RuntimeException("Failed to fetch user", e);
-        }
-    }
-
-    public void deleteUser(String username) {
-        try {
-            usersTable.deleteItem(r -> r.key(k -> k.partitionValue(username)));
-        } catch (DynamoDbException e) {
-            throw new RuntimeException("Failed to delete user", e);
-        }
-    }
+    void deleteUser(String username);
 }
