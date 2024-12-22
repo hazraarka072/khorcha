bbda5380530d5d1a32c3b52c4ca1a6f9ac7a8e5b
Chiradip Maji <chiradip.svts@gmail.com>
2024-12-22 17:54:07 +0530

    Outputs congnito_user_pool_domain

--- terraform/stateless/outputs.tf
+++ terraform/stateless/outputs.tf
@@ -5,3 +5,7 @@ output "lambda_function_arn" {
 output "api_gateway_url" {
   value = module.kharcha_api.api_gateway_url
 }
+
+output "congnito_user_pool_domain" {
+  value = var.enable_cognito ? module.kharcha_cognito[0].congnito_user_pool_domain : ""
+}
